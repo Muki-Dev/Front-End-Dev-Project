9d9d6ed1cb4601e0fc2cd20d943e84d24ec0440c
Muki Dev <mukidev881@gmail.com>
2024-09-08 23:14:17 +0000

    Writing the setDetails Function

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_IMAGE_TITLE = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+
+function setDetails(){
+	'use strict';
+	var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+	detailImage.setAttribute('src','img/otter3.jpg');
+
+	var detailTitle = document.querySelector(DETAIL_IMAGE_TITLE);
+	detailTitle.textContent = 'You Should Be Dancing';
+}
+
+setDetails();
